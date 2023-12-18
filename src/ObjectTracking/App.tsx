14ef5e1445af6cc8ce22411c67a62d3306b13f27
zcy <congyao119@gmail.com>
2023-12-18 14:16:22 +0100

    finished

--- src/ObjectTracking/App.tsx
+++ src/ObjectTracking/App.tsx
@@ -12,10 +12,12 @@ import * as tags from './classtags.json';
 import * as Comlink from 'comlink';
 import './App.css';
 import PlayerHeader from './Header';
-import { EcoObject, Rect, checkInsideRadius, checkOverlap } from './util';
+import { checkInsideRadius, checkOverlap } from './util';
 import { Particle, Particles } from './Particle';
 import switchImg from '../../assets/switch.png';
-import particleImg from '../../assets/particle.png';
+import switchOffImg from '../../assets/switch-flip.png';
+import particleImg from '../../assets/green-particle.png';
+import altParticleImg from '../../assets/yellow-particle.png';
 
 function App() {
 
@@ -32,14 +34,28 @@ function App() {
   );
 
   const [video, setVideo] = useState<HTMLVideoElement>(null);
-  const [ecoBox, setEcoBox] = useState<Rect>(null);
-  const [handKeypoints, setHandKeypoints] = useState<any[]>([]);
-  const [cocoDetections, setCocoDetections] = useState<cocoSsdPrediction[]>([]);
-  const [stored, setStored] = useState<EcoObject[]>([]);
-  const [storeTags, setStoreTags] = useState<string[]>(['e-waste', 'recyclable']);
+
   const particles = new Particles();
-  const [switchPos, setSwitchPos] = useState<{x: number, y: number}>(null);
-  const [showEcoBox, setShowEcoBox] = useState<boolean>(true);
+  const switchImgObj = new Image();
+  switchImgObj.src = switchImg;
+  const switchOffImgObj = new Image();
+  switchOffImgObj.src = switchOffImg;
+  const particleImgObj = new Image();
+  particleImgObj.src = particleImg;
+  const altParticleImgObj = new Image();
+  altParticleImgObj.src = altParticleImg;
+
+  localStorage.setItem('items', JSON.stringify([]));
+  localStorage.setItem('switchPos', JSON.stringify({x: 0, y: 0}));
+  localStorage.setItem('score', JSON.stringify(20));
+  localStorage.setItem('ecoBox', JSON.stringify({
+    storeTags: ['recyclable', 'e-waste'],
+    status: 'on',
+    x: 0,
+    y: 0,
+    width: 0,
+    height: 0,
+  }));
 
   useEffect(() => {
     async function initCamera() {
@@ -54,20 +70,32 @@ function App() {
   }, [])
 
   const drawHands = (ctx : CanvasRenderingContext2D, hands : any[]) => {
-    if(hands) setHandKeypoints(hands);
-    for (let hand of handKeypoints) {
+    if(!hands) return;
+    const switchPos = JSON.parse(localStorage.getItem('switchPos'));
+    for (let hand of hands) {
       const {keypoints} = hand;
       if(keypoints) {
         // thumb finger tip
+        ctx.beginPath();
         ctx.arc(keypoints[4].x, keypoints[4].y, 10, 0, 2*Math.PI)
+        ctx.stroke(); 
 
         // index finger tip
+        ctx.beginPath();
         ctx.arc(keypoints[8].x, keypoints[8].y, 10, 0, 2*Math.PI)    
         ctx.stroke(); 
         if(switchPos) {
           if(checkInsideRadius(keypoints[4], switchPos, 20) 
               && checkInsideRadius(keypoints[8], switchPos, 20)) {
-            setShowEcoBox(!showEcoBox);
+            console.log('switch');
+            const ecoBox = JSON.parse(localStorage.getItem('ecoBox'));
+            // localStorage.setItem('ecoBox', JSON.stringify({...ecoBox, config: ['organic']}));
+            // showEcoBox = false;
+            // setStoreTags(['e-waste', 'recyclable']);
+            localStorage.setItem('ecoBox', JSON.stringify({
+              ...ecoBox, 
+              storeTags: ['organic']}));
+              // status: ecoBox.status === 'on' ? 'off' : 'on'}));
           }
         }
       }
@@ -77,52 +105,72 @@ function App() {
   const drawEcoBox = (ctx : CanvasRenderingContext2D, code : QRCode) => {
     if(!code) return;
     const { location } = code;
-    const { topLeftCorner, bottomLeftCorner, bottomRightCorner } = location;
+    const { topLeftCorner,topRightCorner, bottomLeftCorner } = location;
 
-    const width = bottomLeftCorner.x - topLeftCorner.x;
-    const height = bottomRightCorner.y - bottomLeftCorner.y;
-    setEcoBox({
-      x: topLeftCorner.x + width / 2,
-      y: topLeftCorner.y + height / 2,
-      width,
-      height,
-    })
-    setSwitchPos({
-      x: topLeftCorner.x + width / 2,
-      y: topLeftCorner.y + height / 2,
-    })
+    const width = topRightCorner.x - topLeftCorner.x;
+    const height = bottomLeftCorner.y - topLeftCorner.y;
+    const ecoBox = JSON.parse(localStorage.getItem('ecoBox'));
+    if(!ecoBox) {
+      // save ecobox to local storage
+      localStorage.setItem('ecoBox', JSON.stringify({
+        storeTags: ['e-waste', 'recyclable'],
+        status: 'on',
+        x: topLeftCorner.x,
+        y: topLeftCorner.y,
+        width,
+        height,
+      }));
+    } else {
+      localStorage.setItem('ecoBox', JSON.stringify({
+        ...ecoBox,
+        x: topLeftCorner.x,
+        y: topLeftCorner.y,
+        width,
+        height,
+      }));
+    }
+    if(ecoBox.status === 'off') {
+      ctx.drawImage(switchOffImgObj, topRightCorner.x, topLeftCorner.y, 50, 50);
+      return;
+    }
+
     for(let i = 0; i < 5; i++) {
       const particle : Particle = {
-        x: ecoBox.x + Math.random() * 20 - 10,
-        y: ecoBox.y + Math.random() * 20 - 10,
-        vx: Math.random() * 20 - 10,
-        vy: Math.random() * 20 - 10,
+        x: topLeftCorner.x + width / 2 + Math.random() * 20 - 10,
+        y: topLeftCorner.y + Math.random() * 20 - 10,
+        vx: Math.random() * 10 - 5,
+        vy: - Math.random() * 10,
         life: 100,
-        size: 10,
+        size: Math.random() * 20 + 20,
         update: () => {
           particle.x += particle.vx;
           particle.y += particle.vy;
           particle.life -= 1;
         },
         draw: (ctx : CanvasRenderingContext2D) => {
-          ctx.fillText("🌟", particle.x, particle.y);
-          ctx.drawImage(particleImg, particle.x, particle.y, 10, 10);
+          if(ecoBox.storeTags.includes('organic')) {
+            ctx.drawImage(altParticleImgObj, particle.x, particle.y, particle.size, particle.size);
+          } else {
+            ctx.drawImage(particleImgObj, particle.x, particle.y, particle.size, particle.size);
+          }
         }
       };
       particles.addParticle(particle);
     }
-
+    particles.update();
+    particles.draw(ctx);
     ctx.rect(topLeftCorner.x, topLeftCorner.y, width, height);
     ctx.stroke();
-    ctx.drawImage(switchImg, ecoBox.x, ecoBox.y, 50, 50);
-
+    ctx.drawImage(switchImgObj, topRightCorner.x, topRightCorner.y, 50, 50);
+    localStorage.setItem('switchPos', JSON.stringify({x: topRightCorner.x, y: topRightCorner.y}));
   }
 
   const drawCocoObj = (ctx : CanvasRenderingContext2D, detections : cocoSsdPrediction[], frameCount: number) => {
-    if(detections) setCocoDetections(detections);
+    if(!detections) return;
+    const ecoBox = JSON.parse(localStorage.getItem('ecoBox'));
     if(!ecoBox) return;
     const { x, y } = ecoBox;
-    for (let detection of cocoDetections) {
+    for (let detection of detections) {
       const { bbox, class: name } = detection;
       const objRect = {
         x: bbox[0],
@@ -133,16 +181,47 @@ function App() {
 
       if(bbox && tags[name] !== null) {
         ctx.fillText(tags[name], bbox[0], bbox[1]);
+        ctx.fillText(name, bbox[0], bbox[1]-10);
+        ctx.beginPath();
         ctx.rect(bbox[0], bbox[1], bbox[2], bbox[3]);
         ctx.stroke();
 
           if(bbox && tags[name] !== null) {
             if(checkOverlap(ecoBox, objRect)) {
               console.log(name);
-              if (storeTags.includes(tags[name])) {
-                ctx.fillText("✅", x, y);
-                setStored([...stored, {name, ecoTag: tags[name], score: 10}]);
-              } else ctx.fillText("❌", x, y);
+              const items = JSON.parse(localStorage.getItem('items'))
+
+              if (ecoBox.storeTags.includes(tags[name][0])) {
+                console.log(items, name);
+                console.log(items.length === 0, items.includes(name))
+                if((items.length === 0 || !items.includes(name))) {
+                  ctx.fillText("✅✅✅✅✅✅✅", x, y);
+                  for(let i = 0; i < 5; i++) {
+                    const particle : Particle = {
+                      x: ecoBox.x + ecoBox.width / 2 + Math.random() * 20 - 10,
+                      y: ecoBox.y + Math.random() * 20 - 10,
+                      vx: Math.random() * 20 - 10,
+                      vy: - Math.random() * 20,
+                      life: 100,
+                      size: Math.random() * 20 + 10,
+                      update: () => {
+                        particle.x += particle.vx;
+                        particle.y += particle.vy;
+                        particle.life -= 1;
+                      },
+                      draw: (ctx : CanvasRenderingContext2D) => {
+                        ctx.fillText("✅", particle.x, particle.y);
+                      }
+                    };
+                    particles.addParticle(particle);
+                  }
+                  const score = JSON.parse(localStorage.getItem('score'));
+                  localStorage.setItem('score', JSON.stringify(20 + score));
+                  localStorage.setItem('items', JSON.stringify([...items, name]));
+                  document.getElementById('score-bar').style.width = `${(score + 1) % 100}%`;
+                } 
+                // setStored([...stored, {name, ecoTag: tags[name], score: 10}]);
+              } else ctx.fillText("❌❌❌❌❌❌❌", x, y);
             }
           } else continue;
 
@@ -179,7 +258,7 @@ function App() {
   
   return (
     <div>
-      <PlayerHeader score={stored.reduce((sum : number, cur : EcoObject) => sum += cur.score, 0)}/>
+      <PlayerHeader/>
       <div className="canvas-container">
         {video ? <Canvas draw={draw} video={video} workers={[cocoWorker, qrWorker, handWorker]}/> : null }
       </div>
--- src/ObjectTracking/Header.tsx
+++ src/ObjectTracking/Header.tsx
@@ -1,33 +1,34 @@
 
 import React, { useEffect, useState } from 'react';
-import boxImg from '../../assets/sample.png';
+import boxImg from '../../assets/ecobox.png';
 import userProfile from '../../assets/user-profile.svg';
 import star from '../../assets/star.svg';
 import './App.css';
 
-function PlayerHeader({ score } : { score: number }) {
-
+function PlayerHeader() {
+  
 
   return (
     <div className="header">
     <div className="player-container">
             <img src={userProfile} className="player-img"></img>
         <div className="player">
-        Radu
+        Player
         </div>
     </div>
     <div className="profile">
         <img className="profile-img" src={boxImg}></img>
     </div>
-    <dis className="progress-container">
-        <div className="level"> Lv. {Math.floor(score / 100)}</div>
+    <div className="progress-container">
+        <div className="level" id="level-number"> Lv. 0</div>
         <div className="player-container">
             <img src={star} className="star-img"></img>
             <div className="score-bar">
-            <div className="score" style={{width: `${score}%`}}></div>
+            {/* <div className="score" id="score-bar" style={{width: `${score % 100}%`}}></div> */}
+            <div className="score" id="score-bar"></div>
         </div>
     </div>
-    </dis>
+    </div>
     </div>
     )
 }
